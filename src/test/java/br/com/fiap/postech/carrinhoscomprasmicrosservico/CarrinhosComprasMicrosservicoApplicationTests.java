398b154b6656ba96cff9edbfc63163a3d1199240
João Gabriel Campos <jgcamposneto@gmail.com>
2024-03-10 02:27:50 -0300

    Import inicial

---
+++ src/test/java/br/com/fiap/postech/carrinhoscomprasmicrosservico/CarrinhosComprasMicrosservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.carrinhoscomprasmicrosservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhosComprasMicrosservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
