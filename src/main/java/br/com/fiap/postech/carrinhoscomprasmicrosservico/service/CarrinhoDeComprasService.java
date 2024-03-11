dee0233e92f50d39b3dc0ec990cace990334c643
João Gabriel <joao.neto@trt19.jus.br>
2024-03-11 03:09:24 -0300

    Valor total do carrinho no controller

--- src/main/java/br/com/fiap/postech/carrinhoscomprasmicrosservico/service/CarrinhoDeComprasService.java
+++ src/main/java/br/com/fiap/postech/carrinhoscomprasmicrosservico/service/CarrinhoDeComprasService.java
@@ -1,10 +1,13 @@
 package br.com.fiap.postech.carrinhoscomprasmicrosservico.service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,7 +40,10 @@ public class CarrinhoDeComprasService {
 		if(!produtosDisponiveis) {
 			throw new NoSuchElementException("Um ou mais produtos não estão disponíveis");
 		}
-		
+
+		BigDecimal valorTotal = calcularValorTotal(carrinho.getItens());
+		carrinho.setValorTotal(valorTotal);
+
 		return carrinhosDeComprasRepository.save(carrinho);
 	}
 	
@@ -66,11 +72,38 @@ public class CarrinhoDeComprasService {
 					}
 					
 				} catch (IOException e) {
-					// TODO: handle exception
 				}
 			}
 		}
 		
 		return true;
-	}	
+	}
+
+	private BigDecimal calcularValorTotal(List<ItemPedido> itens) {
+		BigDecimal valorTotal = BigDecimal.ZERO;
+
+		for (ItemPedido item : itens) {
+			Long idProduto = item.getIdProduto();
+			int quantidade = item.getQuantidade();
+
+			ResponseEntity<String> response = restTemplate.getForEntity(
+					"http://localhost:8081/produtos/{id}",
+					String.class,
+					idProduto
+			);
+
+			if(response.getStatusCode() == HttpStatus.OK) {
+                try {
+                    JsonNode produtoJsonNode = objectMapper.readTree(response.getBody());
+					BigDecimal preco = produtoJsonNode.get("preco").decimalValue();
+					valorTotal = valorTotal.add((preco.multiply(BigDecimal.valueOf(quantidade))));
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+
+		}
+		return valorTotal;
+	}
 }
