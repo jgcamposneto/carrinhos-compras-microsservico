4600cef14ef975169a433b1810dbe0e105ad9972
João Gabriel <joao.neto@trt19.jus.br>
2024-03-11 02:35:14 -0300

    Mapeamento da url do controller

--- src/main/java/br/com/fiap/postech/carrinhoscomprasmicrosservico/controller/CarrinhoDeComprasController.java
+++ src/main/java/br/com/fiap/postech/carrinhoscomprasmicrosservico/controller/CarrinhoDeComprasController.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,13 @@ import br.com.fiap.postech.carrinhoscomprasmicrosservico.entity.CarrinhoDeCompra
 import br.com.fiap.postech.carrinhoscomprasmicrosservico.service.CarrinhoDeComprasService;
 
 @RestController
-@RequestMapping("/carrinho")
+@RequestMapping("/carrinhos")
 public class CarrinhoDeComprasController {
 
 	@Autowired
 	private CarrinhoDeComprasService carrinhoDeComprasService;
-	
+
+	@PostMapping
 	public ResponseEntity<?> criarCarrinho(@RequestBody CarrinhoDeCompras carrinho) {
 		try {
 			CarrinhoDeCompras novoCarrinho = carrinhoDeComprasService.criarCarrinho(carrinho);
